d1479c7edccfa77311d5ddc88474eeb7a9c66df7
kbvarma333@gmail.com <kbvarma333@gmail.com>
2021-11-05 04:09:38 +0530

    target folder deleted

--- target/classes/import_data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO public.user values (-1, 'john', 0);
-INSERT INTO public.user values (-2, 'alex', 0);
-INSERT INTO public.user values (-3, 'nick', 0);
-INSERT INTO public.user values (-4, 'alan', 0);
